bbece6654eab274e9d976be0493daf183cb460cd
vickyzguzman01 <62065728+vickyzguzman01@users.noreply.github.com>
2020-03-31 16:03:51 -0500

    Update Mensaje.php

--- Mensaje.php
+++ Mensaje.php
@@ -1,5 +1,5 @@
 <?php
     $nombres = $_POST['nombres'];
     $apellidos = $_POST['apellidos'];
-    echo "Hola Señor(a) ".$nombres." ".$apellidos." bienvenido(a) a la pagina de la empresa Hola Mundo";
+    echo "Hola Señor(a) ".$nombres." ".$apellidos."  bienvenido(a) a la pagina de la empresa Hola Mundo";
 ?>
