4ee8296f91cbaf60fb6cad18c292a52adba19beb
yulianams0308 <62065686+yulianams0308@users.noreply.github.com>
2020-03-31 16:23:42 -0500

    Update Mensaje.php

--- Mensaje.php
+++ Mensaje.php
@@ -1,5 +1,5 @@
 <?php
-    $nombres = $_POST['nombres'];
+    $nombre = $_POST['nombres'];
     $apellidos = $_POST['apellidos'];
     echo "Hola Señor(a) ".$nombres." ".$apellidos."  bienvenido(a) a la pagina de la empresa Hola Mundo";
 ?>
