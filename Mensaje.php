6bca707432634600df8e9704026403918ec3129e
Juan José Herran Rojas <34010628+juanjosehr14@users.noreply.github.com>
2020-03-31 16:58:18 -0500

    Update Mensaje.php

--- Mensaje.php
+++ Mensaje.php
@@ -1,5 +1,5 @@
 <?php
     $nombres = $_POST['nombres'];
     $apellidos = $_POST['apellidos'];
-    echo "Hola Señor(a) ".$nombres." ".$apellidos."  bienvenido(a) a la pagina de la empresa Hola Mundo";
+    echo "<h2><center>Hola Señor(a) ".$nombres." ".$apellidos."  bienvenido(a) a la pagina de la empresa Hola Mundo</center></h2>";
 ?>
